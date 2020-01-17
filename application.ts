a18f3b72da6292a4f018b99140b7112504f1137a
Everson Silva <eversonsilva9799@gmail.com>
2020-01-17 18:35:06 -0300

    polimorfism - LSP

--- application.ts
+++ application.ts
@@ -1,19 +1,6 @@
-import { Cachorro } from './classes/Cachorro';
+import { Animal } from './classes/Animal';
 import { Gato } from './classes/Gato';
 
-let cachorro = new Cachorro('Cachorro', 2);
-let gato = new Gato('gato', 1);
+let animal: Animal = new Gato('gato mané', 1);
 
-console.log(`Eu sou ${cachorro.getName()} e tenho ${cachorro.getIdade()} anos`);
-console.log(cachorro.getEspecie());
-console.log(cachorro.fazerBarulho());
-console.log(cachorro.comer());
-cachorro.brincar();
-
-console.log();
-
-console.log(`Eu sou ${gato.getName()} e tenho ${gato.getIdade()} anos`);
-console.log(gato.getEspecie());
-console.log(gato.fazerBarulho());
-console.log(gato.comer());
-gato.brincar();
+console.log(`${animal.getName()}: ${animal.fazerBarulho()}`);
