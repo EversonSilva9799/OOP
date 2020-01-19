ce816573d5a26939765bd805c282ca8bed0761c0
Everson Silva <eversonsilva9799@gmail.com>
2020-01-18 22:34:54 -0300

    using composition

--- application.ts
+++ application.ts
@@ -1,6 +1,22 @@
 import { Animal } from './classes/Animal';
+import { Cachorro } from './classes/Cachorro';
 import { Gato } from './classes/Gato';
+import { Rato } from './classes/Rato';
+import { Zoologico } from './classes/Zoologico';
 
-let animal: Animal = new Gato('gato mané', 1);
+let zoologico: Zoologico = new Zoologico();
+zoologico.setName('ZOO-123');
 
-console.log(`${animal.getName()}: ${animal.fazerBarulho()}`);
+let gato: Animal = new Gato('mané', 1);
+let cachorro: Animal = new Cachorro('zé', 2);
+let rato: Animal = new Rato('tonho', 2);
+
+zoologico.adicionarAnimal(gato);
+zoologico.adicionarAnimal(cachorro);
+zoologico.adicionarAnimal(rato);
+
+zoologico.listarAnimais();
+
+zoologico.removerAnimal(0);
+
+zoologico.listarAnimais();
